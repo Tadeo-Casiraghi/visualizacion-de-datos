8c20b844274b125190f0155da6ca3133351d07a6
Tadeo <tadeo.casiraghi@gmail.com>
2024-08-25 00:21:58 -0300

    first exploration

---
+++ extractor.py
@@ -0,0 +1,105 @@
+import pandas as pd
+
+columns = ['edad',
+           'total:poblacion',
+           'total:asistir',
+           'sin_instruccion',
+           'primaria:total',
+           'primaria:incompleto',
+           'primaria:completo',
+           'EGB:total',
+           'EGB:incompleto',
+           'EGB:completo',
+           'secundaria:total',
+           'secundaria:incompleto',
+           'secundaria:completo',
+           'polimodal:total',
+           'polimodal:incompleto',
+           'polimodal:completo',
+           'terciario_nu:total',
+           'terciario_nu:incompleto',
+           'terciario_nu:completo',
+           'universitario:total',
+           'universitario:incompleto',
+           'universitario:completo',
+           'postgrado:total',
+           'postgrado:incompleto',
+           'postgrado:completo']
+
+class SheetExtractor:
+
+    def __init__(self, path, sheet):
+        self.path = path
+        self.df = pd.read_excel(self.path, engine = 'openpyxl', sheet_name=sheet)
+        self.name = self.get_name()
+
+        self.total = self.df.iloc[5:48,1:(1+len(columns))]
+        self.total = self.rename_columns(self.total)
+        self.total_ranges = self.get_ranges(self.total)
+        
+        self.females = self.df.iloc[49:92,1:(1+len(columns))]
+        self.females = self.rename_columns(self.females)
+        self.females_ranges = self.get_ranges(self.females)
+
+        self.males = self.df.iloc[93:136,1:(1+len(columns))]
+        self.males = self.rename_columns(self.males)
+        self.males_ranges = self.get_ranges(self.males)
+
+    def get_name(self):
+        texto = self.df.iloc[0,0]
+        index1 = texto.find(',')+2
+        index2 = texto.find('.', index1)
+        return texto[index1:index2]
+
+    def rename_columns(self, df):
+        new_columns = {}
+        for i in range(len(columns)):
+            new_columns['Unnamed: '+str(i+1)] = columns[i]
+        return df.rename(columns=new_columns)
+
+    def get_ranges(self, df):
+        filtered_df = df[df['edad'].str.contains('-')]
+        return filtered_df
+
+    def get(self, table, category, ponderated = False, ranged = True, start_from = None):
+        if table == 'total' and ranged:
+            df = self.total_ranges
+        elif table == 'total':
+            df = self.total
+        elif table == 'females' and ranged:
+            df = self.females_ranges
+        elif table == 'females':
+            df = self.females
+        elif table == 'males' and ranged:
+            df = self.males_ranges
+        else:
+            df = self.males
+        
+        flag = True
+        if start_from:
+            flag = False
+        
+        data = {}
+
+        level = category.split(':')[0]
+
+        for _, row in df.iterrows():
+            if not flag:
+                if row['edad'] == start_from:
+                    flag = True
+                else:
+                    continue
+            if not (str(row[category]).isnumeric()):
+                value = 0
+            else:
+                value = row[category]
+            if ponderated:
+                if not (str(row[level+':total']).isnumeric()):
+                    total = 1
+                else:
+                    total = row[level+':total']
+                data[row['edad']] = value/total
+            else:
+                data[row['edad']] = value
+        
+        return data
---
+++ test.py
@@ -0,0 +1,48 @@
+from extractor import SheetExtractor
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
+import os
+
+directory_path = 'datos_educacion'
+all_files = os.listdir(directory_path)
+
+names = {}
+for file in all_files:
+    if '_tp_' in file or '_tdf_' in file:
+        continue
+    names[file.split('_')[1]] = os.path.join(directory_path, file)
+
+plt.figure(figsize=(12, 8))
+
+counter = 1
+for name, path in names.items():
+
+    print(name, counter)
+    counter += 1
+    excel_file = pd.ExcelFile(path)
+
+    dataset = {}
+    for sheet in range(3, len(excel_file.sheet_names)):
+        test = SheetExtractor(path, sheet)
+        data = test.get('total', 'secundaria:completo', ponderated = True, ranged = True, start_from='25-29')
+        dataset[test.name] = data
+
+    df = pd.DataFrame(dataset)
+    df = df.transpose()
+
+    # Plot the heatmap
+    # plt.figure(figsize=(12, 8))
+    # sns.heatmap(df, cmap="YlGnBu", annot=True, fmt=".2f")
+    # plt.title("Heatmap of Categories by Item and Number Range")
+    # plt.xlabel("Item")
+    # plt.ylabel("Number Range")
+
+
+    average_per_number_range = df.mean(axis= 0)
+
+    # plt.figure(figsize=(12, 8))
+    plt.plot(average_per_number_range, label = name)
+
+plt.legend()
+plt.show()
